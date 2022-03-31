8843aee4077da1fcc6af75614d1960b75d2c160c
wilson <wilson.evangelista22@gmail.com>
2022-03-31 01:48:09 -0300

    Finalizado o projeto

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { themeContext } from "../../Context";
+import "./Experience.css";
+const Experience = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="experience" id='experience'>
+      <div className="achievement">
+        {/* darkMode */}
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>1+</div>
+        <span  style={{color: darkMode?'white':''}}>anos </span>
+        <span>Experience</span>
+      </div>
+      <div className="achievement">
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>20+</div>
+        <span  style={{color: darkMode?'white':''}}>concluído </span>
+        <span>Projetos</span>
+      </div>
+      <div className="achievement">
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>5+</div>
+        <span  style={{color: darkMode?'white':''}}>companhias </span>
+        <span>Trabalhar</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
